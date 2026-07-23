015e3e78f64839bf8c5d18bbb60eb8ae2d93366b
umayer10-AI <mdumayer10@gmail.com>
2026-07-23 20:11:41 +0600

    add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,7 +9,7 @@ import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <main>
+    <main className='bg-[#010925]'>
       <Navbar />
       <Hero />
       <About />
